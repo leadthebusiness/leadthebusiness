199519e623f8dc4e167a1dd3aa6f17ef7086ef6c
Harsh Sawant <harshsawant2505@gmail.com>
2025-07-20 02:10:54 +0530

    change repo

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -32,6 +32,8 @@ export default function Navigation() {
       transition={{ duration: 0.8 }}
       className="fixed top-0 w-full z-50 bg-black/90 backdrop-blur-xl border-b border-yellow-500/20"
     >
+
+      {/*  */}
       {/*  */}
       <div className="container mx-auto px-3 sm:px-4">
         <div className="flex items-center justify-between h-16 sm:h-20">
